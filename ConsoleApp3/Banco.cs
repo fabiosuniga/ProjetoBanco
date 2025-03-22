76cb90f982b5358aa2a9495a42d10eea9d4220fa
fabiosuniga <fabiosuniga@hotmail.com>
2025-03-22 19:07:43 -0300

    Add project files.

---
+++ ConsoleApp3/Banco.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Banco
+    {
+
+        private string _nomecliente;
+        private int _numeroconta;
+        public double Saldo { get; private set; }
+
+
+        public Banco(string nomecliente, int numeroconta)
+        {
+            SetNomeCliente(nomecliente);
+            SetNumeroConta(numeroconta);
+
+        }
+
+        public Banco(string nomecliente, int numeroconta, double saldo) : this(nomecliente, numeroconta)
+        {
+            Saldo = saldo;
+        }
+
+        public string GetNomeCliente()
+
+        {
+            return _nomecliente;
+        }
+
+        public void SetNomeCliente(string nome)
+        {
+            if (nome != null && nome.Length > 1)
+            {
+                _nomecliente = nome;
+            }
+        }
+
+        public int GetNumeroConta()
+
+        {
+            return _numeroconta;
+        }
+        public void SetNumeroConta(int numero)
+        {
+            if (numero != null || numero.ToString().Length == 4)
+            {
+                _numeroconta = numero;
+            }
+        }
+
+        public void Deposito(double valor)
+        {
+            Saldo += valor;
+        }
+
+        public void Saque(double valor)
+        {
+            Saldo -= valor + 5.00;
+        }
+
+        public override string ToString()
+        {
+            return "O seu nome é: "
+                + _nomecliente
+                + ", o número da sua conta é: "
+                + _numeroconta
+                + ", o seu saldo atual é: R$"
+                + Saldo.ToString("F2");
+
+            ;
+        }
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Banco pessoa;
+            char inicial; string nome; int numeroconta; double valor; double deposito, saque;
+
+            Console.Write("Você deseja fazer um saque inicial? s/n : ");
+            inicial = char.Parse(Console.ReadLine());
+
+            if (inicial == 's' || inicial == 'S')
+            {
+                Console.Write("Digite seu nome: ");
+                nome = Console.ReadLine();
+                Console.Write("Digite o numero da conta: ");
+                numeroconta = int.Parse(Console.ReadLine());
+                Console.Write("Digite o valor do deposito inicial: ");
+                valor = double.Parse(Console.ReadLine());
+                pessoa = new Banco(nome, numeroconta, valor);
+
+            }
+
+            else
+            {
+                Console.Write("Digite seu nome: ");
+                nome = Console.ReadLine();
+                Console.Write("Digite o numero da conta: ");
+                numeroconta = int.Parse(Console.ReadLine());
+                pessoa = new Banco(nome, numeroconta);
+            }
+
+            Console.WriteLine(pessoa);
+            Console.WriteLine();
+
+            Console.Write("Digite quantos reais você quer depositar na conta: ");
+            deposito = double.Parse(Console.ReadLine());
+            pessoa.Deposito(deposito);
+
+            Console.WriteLine(pessoa);
+            Console.WriteLine();
+
+            Console.Write("Digite quantos reais você sacar da conta: ");
+            saque = double.Parse(Console.ReadLine());
+            pessoa.Saque(saque);
+
+            Console.WriteLine(pessoa);
+            Console.WriteLine();
+
+        }
+    }
+}
